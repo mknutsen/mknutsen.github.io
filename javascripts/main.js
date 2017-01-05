8dbf4d386b348b11c3086cc526dfb6def7e4ae2d
Max Knutsen <mknutsen99@gmail.com>
2017-01-05 09:28:28 -0500

    js update instead of a link

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+function showDiv(what) {
+    if(document.getElementById('main').style.display == "") {
+        document.getElementById('main').style.display = "none";
+        document.getElementById('projects').style.display = "";
+        what.src = 'images/back.png';
+
+    } else{
+        document.getElementById('main').style.display = "";
+        document.getElementById('projects').style.display = "none";
+        what.src = 'images/portfolio3.png';
+    }
+}
