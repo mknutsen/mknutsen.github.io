7d9052be62c6c3c7689a22efdfba9cc8423e955d
Max Knutsen <mknutsen99@gmail.com>
2017-10-22 18:29:25 -0700

    no more words
    also switched youtube to gyfcat

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,15 @@
 function showDiv(what) {
-    if(document.getElementById('main').style.display == "") {
-        $('#main').slideToggle("slow",function(){
-        	$('#projects').slideToggle("slow");
-        });
+    // if(document.getElementById('main').style.display == "") {
+    //     $('#main').slideToggle("slow",function(){
+    //     	$('#projects').slideToggle("slow");
+    //     });
 
-    } else{
-        $('#projects').slideToggle("slow",function(){
-        	$('#main').slideToggle("slow");
-        });
-    }
+    // } else{
+    //     $('#projects').slideToggle("slow",function(){
+    //     	$('#main').slideToggle("slow");
+    //     });
+    // }
+    $('#projects').slideToggle("slow");
 
     $(what).find('i').toggleClass('fa-folder-o fa-folder-open-o');
 }
