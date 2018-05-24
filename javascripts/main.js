aa12776ac0ee852c350eb5484a0804dc3b148918
Max Knutsen <mknutsen99@gmail.com>
2018-05-24 06:27:35 -0700

    copy email to clipboard

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+    console.log("please hire me");
+    var clipboard = new ClipboardJS('.email', {
+            text: function() {
+                return 'mknutsen99@gmail.com';
+            }
+        });
+
+        clipboard.on('success', function(e) {
+            var element = document.getElementsByClassName("email")[0]
+            $(element).attr('title', 'Copied!')
+                .tooltip('fixTitle')
+                .tooltip('show');
+        });
+
+        clipboard.on('error', function(e) {
+        });
+});
+
 function showDiv(what) {
     // if( $(what).hasClass('fa-folder-o')) {
     // //     $('#main').slideToggle("slow",function(){
@@ -25,4 +45,5 @@ function showDiv(what) {
     // var current = document.getElementById("myHeader").style.paddingTop;
     // document.getElementById("myHeader").style.paddingTop = "40%";
 }
+
 //<i class="fa fa-undo fa-3x"></i>
